1683baac71d994cbbcc108076b926119534ae63d
Zainen <zainensuzuki@gmail.com>
2024-03-25 14:30:55 -0700

    stage 3 fully working

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,83 +1,92 @@
-use std::collections::HashMap;
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 
-use crate::models::*;
-use axum::{ http::StatusCode, response::IntoResponse, Json };
-use chrono::Utc;
-use uuid::Uuid;
+use crate::{models::*, persistance::answers_dao, AppState};
+
+mod handlers_inner;
+
+impl IntoResponse for handlers_inner::HandlerError {
+  fn into_response(self) -> axum::response::Response {
+      match self {
+          handlers_inner::HandlerError::BadRequest(msg) => {
+              (StatusCode::BAD_REQUEST, msg).into_response()
+          }
+          handlers_inner::HandlerError::InternalError(msg) => {
+              (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+          }
+      }
+  }
+}
 
 // ---- CRUD for Questions ----
 
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-  let Question { title, description } = question;
-  let question_uuid = Uuid::new_v4().to_string();
-  let created_at = Utc::now().to_string();
-  let question_detail = QuestionDetail {
-    question_uuid,
-    title,
-    description,
-    created_at,
-  };
-  (StatusCode::OK, Json(question_detail))
+pub async fn create_question(
+    // Example of how to add state to a route. Note that we are using ".." to ignore the other fields in AppState.
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question): Json<Question>,
+) -> impl IntoResponse {
+    let questions_dao = questions_dao.as_ref();
+    let question = handlers_inner::create_question(question, questions_dao).await;
+    match question {
+      Ok(question) => Json(question).into_response(),
+      Err(error) => error.into_response(),
+    }
 }
 
-pub async fn read_questions() -> impl IntoResponse {
-    (StatusCode::OK, Json::<Vec<QuestionDetail>>(vec![
-      QuestionDetail {
-          question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_owned(),
-          title: "Newly Created Question".to_owned(),
-          description: "My Description".to_owned(),
-          created_at: "2022-12-31 18:44:08.287442".to_owned()
-      }
-  ]))
+pub async fn read_questions(// TODO: add questions_dao from app state as an argument
+  State(AppState { questions_dao, ..}): State<AppState> 
+) -> impl IntoResponse {
+  let questions = handlers_inner::read_questions(questions_dao.as_ref()).await;
+  match questions {
+    Ok(questions) => Json(questions).into_response(),
+    Err(error) => error.into_response(),
+  }
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-  let QuestionId { question_uuid } = question_uuid;
-  let mut questions: HashMap<String, QuestionDetail> = HashMap::new();
-  questions.insert(question_uuid.clone(), QuestionDetail {
-    created_at: Utc::now().to_string(), 
-    description: "description".to_owned(), 
-    title: "title".to_owned(), 
-    question_uuid: question_uuid.clone()
-  });
-  questions.remove(&question_uuid);
-
+pub async fn delete_question(
+    // TODO: add questions_dao from app state as an argument
+    State(AppState { questions_dao, ..}): State<AppState>,
+    Json(question_uuid): Json<QuestionId>,
+) -> impl IntoResponse {
+    match handlers_inner::delete_question(question_uuid, questions_dao.as_ref()).await {
+      Ok(_) => StatusCode::OK,
+      Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
 }
 
 // ---- CRUD for Answers ----
 
-pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
-    let Answer { content, question_uuid } = answer;
-    let answer_uuid = Uuid::new_v4().to_string();
-    let created_at = Utc::now().to_string();
-    let answer_detail = AnswerDetail {
-      answer_uuid,
-      content,
-      question_uuid,
-      created_at,
-    };
-    (StatusCode::OK, Json(answer_detail))
-}
-
-pub async fn read_answers() -> impl IntoResponse {
-  (StatusCode::OK, Json::<Vec<AnswerDetail>>(vec![
-    AnswerDetail {
-        answer_uuid: "a1a14a9c-ab9e-481b-8120-67f675531ed2".to_owned(),
-        created_at: "2022-12-31 18:44:08.287442".to_owned(),
-        content: "Test question".to_owned(),
-        question_uuid: "d347261c-3f0e-42d2-8706-5ef9f1b96725".to_owned()
+pub async fn create_answer(
+    // Example of how to add state to a route
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer): Json<Answer>,
+) -> impl IntoResponse {
+    let answers_dao = answers_dao.as_ref();
+    let answer = handlers_inner::create_answer(answer, answers_dao).await;
+    match answer {
+      Ok(answer) => Json(answer).into_response(),
+      Err(error) => error.into_response(),
     }
-]))
 }
 
-pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {
-  let AnswerId { answer_uuid } = answer_uuid;
-  let mut answers: HashMap<String, AnswerDetail> = HashMap::new();
-  answers.insert(answer_uuid.clone(), AnswerDetail {
-    created_at: Utc::now().to_string(), 
-    content: "content".to_owned(), 
-    question_uuid: "".to_owned(),
-    answer_uuid: answer_uuid.clone()
-  });
-  answers.remove(&answer_uuid);
+pub async fn read_answers(
+    // TODO: add answers_dao from app state as an argument
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(question_uuid): Json<QuestionId>,
+) -> impl IntoResponse {
+  let answers = handlers_inner::read_answers(question_uuid, answers_dao.as_ref()).await;
+  match answers {
+    Ok(answers) => Json(answers).into_response(),
+    Err(error) => error.into_response(),
+  }
+}
+
+pub async fn delete_answer(
+    // TODO: add answers_dao from app state as an argument
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer_uuid): Json<AnswerId>,
+) -> impl IntoResponse {
+  match handlers_inner::delete_answer(answer_uuid, answers_dao.as_ref()).await {
+    Ok(_) => StatusCode::OK,
+    Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 extern crate pretty_env_logger;
 
 #[macro_use] extern crate log;
+use std::sync::Arc;
+
 use dotenvy::dotenv;
+use persistance::{answers_dao::AnswersDao, questions_dao::QuestionsDao};
 use sqlx::postgres::PgPoolOptions;
 use axum::{
     routing::{delete, get, post},
@@ -15,6 +18,13 @@ mod persistance;
 
 use handlers::*;
 
+use crate::persistance::{answers_dao::AnswersDaoImpl, questions_dao::QuestionsDaoImpl};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -31,13 +41,13 @@ async fn main() {
      .await
      .expect("failed to connect to database");
 
-    // let recs = sqlx::query!(r#"SELECT * FROM questions"#)
-    // .fetch_all(&pool)
-    // .await
-    // .unwrap();
-
-    // info!("********* Question Records *********");
-
+     let questions_dao = QuestionsDaoImpl::new(pool.clone());
+     let answers_dao = AnswersDaoImpl::new(pool.clone()); // create a new instance of AnswersDaoImpl passing in `pool`
+ 
+     let app_state = AppState {
+        questions_dao: Arc::new(questions_dao),
+        answers_dao: Arc::new(answers_dao),
+     }; // create a new instance of AppState
 
     let app = Router::new()
         .route("/question", post(create_question))
@@ -45,7 +55,8 @@ async fn main() {
         .route("/question", delete(delete_question))
         .route("/answer", post(create_answer))
         .route("/answers", get(read_answers))
-        .route("/answer", delete(delete_answer));
+        .route("/answer", delete(delete_answer))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
